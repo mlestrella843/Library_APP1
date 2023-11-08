f58aa3fd744a9c2f256f1bae7eef525f818c8772
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-11-08 07:34:54 -0700

    CRUD finished

--- server/routes.js
+++ server/routes.js
@@ -1,17 +1,62 @@
 const express = require('express');
 
 const routes = express.Router();
-
+//* Get a book
 routes.get('/', (req,res) => {
     // res.send('GET request received');
     req.getConnection((err, conn) => {
         if(err) return res.send(err)
 
+        conn.query('SELECT * FROM books', (err, rows) => {
+            if(err) return res.send(err)
 
-
-
+            res.json(rows)
+        })
     })
     
+});
+
+//* Create a Book
+routes.post('/', (req,res) => {
+    // res.send('GET request received');
+    req.getConnection((err, conn) => {
+        if(err) return res.send(err)
+       // console.log(req.body);
+        conn.query('INSERT INTO books set ?',[req.body],(err, rows) => {
+            if(err) return res.send(err)
+
+            res.send('book was inserted');
+        })
+    })
+    
+});
+
+//* Delete a Book
+routes.delete('/:id', (req,res) => {
+    // res.send('GET request received');
+    req.getConnection((err, conn) => {
+        if(err) return res.send(err)
+       // console.log(req.body);
+        conn.query('DELETE FROM books WHERE id = ?',[req.params.id],(err, rows) => {
+            if(err) return res.send(err)
+
+            res.send('book was excluded');
+        })
+    })  
+});
+
+//* Edit a Book
+routes.put('/:id', (req,res) => {
+    // res.send('GET request received');
+    req.getConnection((err, conn) => {
+        if(err) return res.send(err)
+       // console.log(req.body);
+        conn.query('UPDATE books set ? WHERE id = ?',[ req.body, req.params.id],(err, rows) => {
+            if(err) return res.send(err)
+
+            res.send('book was edited or updated');
+        })
+    })  
 });
 
 module.exports = routes;
