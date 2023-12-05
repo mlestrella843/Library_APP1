f19427f3882113da4a362abbfaad4e8ec4bc4f42
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-12-05 12:07:41 -0700

    Adding auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,34 @@
+
+
+
+const dbOptions = {
+    host: process.env.DATABASE_HOST,
+    port: 3306,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+}
+
+const mysql = require('mysql');
+
+const myconn = require('express-myconnection');
+
+app.use(myconn(mysql, dbOptions, 'single'));
+
+
+
 exports.register = (req,res) => {
     console.log(req.body);
+
+    const { name, email, password, pass_confirm } = req.body;
+
+    dbOptions.query()
+
     res.send("Form Submitted");
+
+
+
+
+
+
 }
